d51770bb50135f6242a8f20b805d1e8fda0e0631
unknown <Davidyuan@vesync.local>
2024-10-09 22:06:22 +0800

    update

--- testcase/test_sd_handler.py
+++ testcase/test_sd_handler.py
@@ -7,12 +7,14 @@ class TestStableDiffusionHandler(unittest.TestCase):
     def setUp(self):
         self.handler = StableDiffusionHandler()
         self.test_prompt = "A beautiful sunset over the ocean"
-        self.test_output_path = "tests/output/test_image.png"
+        self.test_output_path = os.path.join(os.getcwd(), "tests", "output", "test_image.png")
+        self.temp_image_path = os.path.join(os.getcwd(), "temp", "temp.png")
 
     def test_generate_image(self):
         image = self.handler.generate_image(self.test_prompt)
         self.assertIsInstance(image, Image.Image)
         self.assertEqual(image.size, (512, 512))  # 默认图像大小
+        self.assertTrue(os.path.exists(self.temp_image_path))
 
     def test_save_image(self):
         image = self.handler.generate_image(self.test_prompt)
@@ -24,6 +26,8 @@ class TestStableDiffusionHandler(unittest.TestCase):
     def tearDown(self):
         if os.path.exists(self.test_output_path):
             os.remove(self.test_output_path)
+        if os.path.exists(self.temp_image_path):
+            os.remove(self.temp_image_path)
 
 if __name__ == '__main__':
     unittest.main()
