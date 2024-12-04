2ae96fa48ae0f5bb562186b39af88477d8cc0adf
davidyuan666 <wu.xiguanghua2014@gmail.com>
2024-12-04 23:49:48 +0800

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 sudo apt update
 sudo apt install ffmpeg
-pip install -r requirements.txt
-featurize port export 5000
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+source ~/.bashrc
+conda --version
+# pip install -r requirements.txt
+# featurize port export 5000
