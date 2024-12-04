20c0506259fb526b0174949a632019c86065c59c
davidyuan666 <wu.xiguanghua2014@gmail.com>
2024-12-05 00:18:30 +0800

    update

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
 source ~/.bashrc
 conda --version
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 # pip install -r requirements.txt
 # featurize port export 5000
